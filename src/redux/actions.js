857a9f4e3d555e0d8af783aceb48c1b81253c0d5
gitkiselev <482003@mail.ru>
2020-05-19 16:34:12 +0300

    Routing implemented

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -3,7 +3,8 @@ import {
   CHANGE_STYLES,
   TABLE_RESIZE,
   APPLY_STYLE,
-  CHANGE_TITLE
+  CHANGE_TITLE,
+  UPDATE_DATE
 } from './types'
 
 // Action Creator
@@ -21,6 +22,11 @@ export function changeText(data) {
   }
 }
 
+export function updateDate() {
+  return {
+    type: UPDATE_DATE
+  }
+}
 export function changeStyles(data) {
   return {
     type: CHANGE_STYLES,
